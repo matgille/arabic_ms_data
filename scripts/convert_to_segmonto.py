6c2d9c3bdb344827d7016de03fb62f8a8c883c8e
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-09-11 01:04:17 +0200

    Add segmonto mapping scripts, update files

---
+++ scripts/convert_to_segmonto.py
@@ -0,0 +1,45 @@
+import lxml.etree as ET
+import glob
+import sys
+import json
+
+alto = {'alto': 'http://www.loc.gov/standards/alto/ns-v4#'}
+alto_ns_cr = '{http://www.loc.gov/standards/alto/ns-v4#}'
+
+
+def main():
+    with open("typologie.json") as input_file:
+        json_mapping = json.load(input_file)
+
+    for file in glob.glob(f"*/*/*.xml"):
+        if "segmonto" in file:
+            continue
+        as_tree = ET.parse(file)
+        all_tags = as_tree.xpath("//alto:Tags/alto:OtherTag", namespaces=alto)
+        all_labels = as_tree.xpath("//alto:Tags/alto:OtherTag/@LABEL", namespaces=alto)
+        all_ids = as_tree.xpath("//alto:Tags/alto:OtherTag/@ID", namespaces=alto)
+        all_labels_and_tags = list(zip(all_tags, all_labels, all_ids))
+        tags_element = as_tree.xpath("//alto:Tags", namespaces=alto)
+        default_tag = ET.Element(alto_ns_cr + "OtherTag")
+        default_tag.set("LABEL", "DefaultLine")
+        default_tag.set("ID", "LT0")
+        tags_element.append(default_tag)
+        for element, label, ident in all_labels_and_tags:
+            replacement = json_mapping[ident[0]][label]
+            if replacement == "None":
+                element.getparent().remove(element)
+            else:
+                element.set("LABEL", replacement)
+
+        # On s'occupe des lignes sans typologie
+        all_lines = as_tree.xpath("//alto:TextLine[not(@TAGREFS)]", namespaces=alto)
+        for line in all_lines:
+            line.set("TAGREFS", "LT0")
+
+        with open(file.replace(".xml", ".segmonto.xml"), "w") as output_xml:
+            output_xml.write(ET.tostring(as_tree, pretty_print=True, encoding="utf-8").decode())
+
+
+if __name__ == '__main__':
+    main()
+
